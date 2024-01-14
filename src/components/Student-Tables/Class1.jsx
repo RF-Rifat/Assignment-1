bdb59aa62d332cdbae4f3174be8f45902163445b
RF-Rifat <rfrifat6344@gmail.com>
2024-01-14 22:20:05 +0600

    table photo style added

--- src/components/Student-Tables/Class1.jsx
+++ src/components/Student-Tables/Class1.jsx
@@ -12,9 +12,9 @@ const Class1 = ({ item }) => {
         <tr key={student.id} className="border-b border-[#7ECEB529]">
               <td className="p-5 text-sm md:text-xl">{student.id}</td>
           <td className="p-5 text-sm md:text-xl">
-            <div className="flex space-x-3 items-center">
+            <div className="flex space-x-3 items-center ">
               <img
-                className="w-8 h-8"
+                className="w-8 h-8 rounded-md"
                 src={student?.image}
                 width={32}
                 height={32}
@@ -31,57 +31,6 @@ const Class1 = ({ item }) => {
           </td>
         </tr>
       ))}
-      {/* <tr className="border-b border-[#7ECEB529]">
-        <td className="p-5 text-sm md:text-xl">1</td>
-        <td className="p-5 text-sm md:text-xl">
-          <div className="flex space-x-3 items-center">
-            <img
-              className="w-8 h-8"
-              src="./assets/avatar.png"
-              width={32}
-              height={32}
-              alt="John Smith"
-            />
-            <span className="whitespace-nowrap">John Smith</span>
-          </div>
-        </td>
-        <td className="p-5 text-sm md:text-xl text-center">A+</td>
-        <td className="p-5 text-sm md:text-xl text-center">98%</td>
-      </tr> */}
-      {/* <tr className="border-b border-[#7ECEB529]">
-        <td className="p-5 text-sm md:text-xl">2</td>
-        <td className="p-5 text-sm md:text-xl">
-          <div className="flex space-x-3 items-center">
-            <img
-              className="w-8 h-8"
-              src="./assets/avatar.png"
-              width={32}
-              height={32}
-              alt="John Smith"
-            />
-            <span className="whitespace-nowrap">John Smith</span>
-          </div>
-        </td>
-        <td className="p-5 text-sm md:text-xl text-center">A+</td>
-        <td className="p-5 text-sm md:text-xl text-center">98%</td>
-      </tr>
-      <tr className="border-b border-[#7ECEB529]">
-        <td className="p-5 text-sm md:text-xl">3</td>
-        <td className="p-5 text-sm md:text-xl">
-          <div className="flex space-x-3 items-center">
-            <img
-              className="w-8 h-8"
-              src="./assets/avatar.png"
-              width={32}
-              height={32}
-              alt="John Smith"
-            />
-            <span className="whitespace-nowrap">John Smith</span>
-          </div>
-        </td>
-        <td className="p-5 text-sm md:text-xl text-center">A+</td>
-        <td className="p-5 text-sm md:text-xl text-center">98%</td>
-      </tr> */}
     </>
   );
 };
